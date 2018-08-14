f4b9670c750474654414ebbd1e518b8b3605190f
mattfro333 <mattfro333@gmail.com>
2018-08-13 23:53:44 -0600

    adding routes for a back end

---
+++ server/routes/items.js
@@ -0,0 +1,13 @@
+module.exports = function (app){
+var items = [{
+    name:"Ice Cream"
+  },{
+    name:"Waffles"
+  },{
+    name:"Candy",
+    purchased:true
+  },{
+    name:"Snacks"
+}];
+  app.route('/api/')
+}
