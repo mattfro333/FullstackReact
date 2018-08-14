da2444d9bfe7ba12657599b30bfd680b3690c8cb
mattfro333 <mattfro333@gmail.com>
2018-08-14 11:36:18 -0600

    added a helper

--- server/main.js
+++ server/main.js
@@ -1,11 +1,14 @@
 const express = require('express');
 
 const app = new express();
-const parser =require('body-parser');
+const parser = require('body-parser');
 app.get('/', function(req,res){
     res.render('./../app/index.ejs', {});
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777)
+
+app.use(parser.json());
+app.use(parser.urlencoded({extended:false}));
 
 require('./routes/items.js')(app);
