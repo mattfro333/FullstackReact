d1e7862cd231de1d215f5a76307fbc7377b47519
mattfro333 <mattfro333@gmail.com>
2018-08-19 22:13:50 -0600

    attempting to make the app isomorphic

--- server/main.js
+++ server/main.js
@@ -2,9 +2,20 @@ const express = require('express');
 
 const app = new express();
 const parser = require('body-parser');
+const React = require('react/addons');
+var GroceryItem = require('./models/GroceryItem.js');
 require('./database.js');
 app.get('/', function(req,res){
-    res.render('./../app/index.ejs', {});
+    // res.render('./../app/index.ejs', {});
+var application = React.createfactory(require('./../app/components/GroceryItemList.jsx'))
+
+      GroceryItem.find(function(error,doc){
+          var generated = React.renderToString(application({
+            items:doc
+          }))
+          res.redner('./../app/index.ejs', {reactOutput:generated});
+      })
+
 })
 .use(express.static(__dirname + '/../.tmp'))
 .listen(7777)
